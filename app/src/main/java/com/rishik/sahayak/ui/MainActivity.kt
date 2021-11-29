7e9374590c7a09ac12617e6bbea3b2cbf102f1c3
Rishik Gupta <rishikgupta8@gmail.com>
2021-11-29 12:39:41 +0530

    Connected Firebase

--- app/src/main/java/com/rishik/sahayak/ui/MainActivity.kt
+++ app/src/main/java/com/rishik/sahayak/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.rishik.sahayak
+package com.rishik.sahayak.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.rishik.sahayak.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
